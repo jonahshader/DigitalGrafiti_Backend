b8d89842f4a18633770fcba8a9358ac330f8a094
Jonah Shader <jonah.shader@gmail.com>
2019-09-28 17:53:03 -0400

    working example of drawing GImage to screen

--- src/difti/MainApp.kt
+++ src/difti/MainApp.kt
@@ -4,18 +4,30 @@ import difti.display.Display
 import processing.core.PApplet
 
 class MainApp : PApplet() {
-    lateinit val display: Display
+    lateinit var display: Display
 
     override fun settings() {
         fullScreen()
     }
 
     override fun setup() {
-
+        display = Display(this, width, height)
+        val tempPixelArray = ArrayList<ArrayList<Pixel>>()
+        for (y in 0 until 480) {
+            tempPixelArray.add(ArrayList())
+            for (x in 0 until 640) {
+                tempPixelArray[y].add(Pixel((Math.random() * 255).toInt(), (Math.random() * 255).toInt(), (Math.random() * 255).toInt(), (Math.random() * 255).toInt()))
+            }
+        }
+        val tempImage = GImage(this, PixelArrayImage(tempPixelArray), width/2f, height/2f, 1f)
+        val tempImage2 = GImage(this, PixelArrayImage(tempPixelArray), width/2f, height/2f, 0f)
+        display.addImage(tempImage)
+        display.addImage(tempImage2)
     }
 
     override fun draw() {
         background(255)
+        display.drawToScreen(this)
     }
 }
 
