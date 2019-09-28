dee37b339a60069e02a1a458c28e0c72f197d1ce
sarenord <ajsheld12@gmail.com>
2019-09-28 16:20:45 -0400

    reorganized

--- src/difti/networkinterface/NetworkInterface.kt
+++ src/difti/networkinterface/NetworkInterface.kt
@@ -8,17 +8,19 @@ import java.net.Socket
 import kotlin.concurrent.thread
 
 //TODO: get out of main function
-fun socketserver() {
-    val server = ServerSocket(1337)
-    while (true) {
-        val client = server.accept()
-        thread {
-            ClientHandler(client).run()
+class socketserver(disp: Display) {
+    fun init() {
+        val server = ServerSocket(1337)
+        while (true) {
+            val client = server.accept()
+            thread {
+                ClientHandler(client).run()
+            }
         }
     }
 }
 
-class ClientHandler(client: Socket, disp: Display) {
+class ClientHandler(client: Socket) {
     private val client: Socket = client
     private var active: Boolean = false
     fun run() {
