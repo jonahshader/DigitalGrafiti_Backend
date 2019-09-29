667f5f9b70f5cdfc44a7a5c1baf19c6054da3e15
sarenord <ajsheld12@gmail.com>
2019-09-29 06:52:01 -0400

    this code might work

--- src/difti/networkinterface/NetworkInterface.kt
+++ src/difti/networkinterface/NetworkInterface.kt
@@ -20,14 +20,14 @@ class SocketServer(val disp: Display, val app: PApplet) {
             while (true) {
                 val client = server.accept()
                 thread {
-                    ClientHandler(client, disp).run()
+                    ClientHandler(client, app, disp).run()
                 }
             }
         }
     }
 }
 
-private class ClientHandler(val client: Socket, val disp: Display) {
+private class ClientHandler(val client: Socket, val app: PApplet, val disp: Display) {
     private var active: Boolean = false
     fun run() {
         active = true
@@ -37,21 +37,21 @@ private class ClientHandler(val client: Socket, val disp: Display) {
         while (l != null) {
             l += br.readLine()
         }
-        disp.addImage(mkImgFromPixArray(l))
+        disp.addImage(mkImgFromPixArray(l, app))
     }
 }
 
 private fun mkImgFromPixArray(obj: JSONObject, app: PApplet) : difti.GImage {
-    val h = obj.getJSONObject("height")
-    val w = obj.getJSONObject("width")
-    val x_center = obj.getJSONObject("x")
-    val y_center = obj.getJSONObject("y")
-    val rotation = obj.getJSONObject("rotation")
+    val h = obj.getFloat("height")
+    val w = obj.getFloat("width")
+    val x_center = obj.getFloat("x")
+    val y_center = obj.getFloat("y")
+    val rotation = obj.getFloat("rotation")
     val pixArray = obj.getJSONArray("rgba")
     var img = ArrayList<ArrayList<Pixel>>()
     var tAr = ArrayList<Pixel>()
     for (i in 0..pixArray.size()-1) {
-        if (i%(pixArray.size()/Integer.parseInt(w.toString())) == 0) {
+        if ((i%(pixArray.size()/w)).equals(0)) {
             img.add(tAr)
             tAr = ArrayList<Pixel>()
         }
@@ -61,5 +61,5 @@ private fun mkImgFromPixArray(obj: JSONObject, app: PApplet) : difti.GImage {
         }
     }
     val i = difti.PixelArrayImage(img)
-    return GImage(app, i, Integer.parseInt(x_center.toString()), Integer.parseInt(y_center.toString(), String.toFloat(rotation.toString())))
+    return GImage(app, i, x_center, y_center, rotation)
 }
