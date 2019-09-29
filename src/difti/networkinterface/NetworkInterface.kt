c0f600da1651c9aff565e0e94b3cf45d63a353f9
sarenord <ajsheld12@gmail.com>
2019-09-28 23:50:59 -0400

    created server and image generation logic

--- src/difti/networkinterface/NetworkInterface.kt
+++ src/difti/networkinterface/NetworkInterface.kt
@@ -1,7 +1,10 @@
 package difti.networkinterface
 
+import difti.Pixel
 import difti.display.Display
-import processing.data.JSONArray
+import processing.data.JSONObject
+import java.io.BufferedReader
+import java.io.InputStreamReader
 import java.util.*
 import java.net.ServerSocket
 import java.net.Socket
@@ -22,17 +25,38 @@ class SocketServer(disp: Display) {
     }
 }
 
-private class ClientHandler(client: Socket) {
-    private val client: Socket = client
+private class ClientHandler(val client: Socket) {
     private var active: Boolean = false
     fun run() {
         active = true
-        while (active) {
-
+        val inp = client.getInputStream()
+        val br = BufferedReader(InputStreamReader(client))
+        var l
+        while (l != null) {
+            l += br.readLine()
         }
+
+        val mkPixArrayImage()
     }
 }
 
-private fun pixArrayImage(obj: JSONArray) {
-
+private fun mkImgFromPixArray(obj: JSONObject) : difti.GImage {
+    val h = obj.getJSONObject('height')
+    val w = obj.getJSONObject('width')
+    val x_center = obj.getJSONObject('x')
+    val y_center = obj.getJSONObject('y')
+    val pixArray = obj.getJSONArray('rgba')
+    var img = ArrayList<ArrayList<Pixel>>()
+    var tAr = ArrayList<Pixel>()
+    for (i in pixArray.indices) {
+        if (i%pixArray.size()/w == 0) {
+            img.add(tAr)
+            tAr = ArrayList<Pixel>()
+        }
+        if (i%4 == 0) {
+           val pix = Pixel(pixArray[i], pixArray[i+1], pixArray[i+2], pixArray[i+3])
+           tAr.add(pixArray[i])
+        }
+    }
+    val i = difti.PixelArrayImage(img)
 }
